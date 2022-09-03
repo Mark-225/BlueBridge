a2f40fbb899beb52b7f494cb493fcf53e25fa969
Mark B <mark@mark225.de>
2022-09-03 19:45:50 +0200

    Add more WorldGuard lookup features

--- BlueBridgeWG/src/main/java/de/mark225/bluebridge/worldguard/util/RegionStringLookup.java
+++ BlueBridgeWG/src/main/java/de/mark225/bluebridge/worldguard/util/RegionStringLookup.java
@@ -16,6 +16,7 @@ import de.mark225.bluebridge.worldguard.addon.WorldGuardIntegration;
 import org.bukkit.Bukkit;
 
 import java.math.BigInteger;
+import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
@@ -81,12 +82,59 @@ public class RegionStringLookup extends StringLookupWrapper {
                     return "[Invalid placeholder syntax]";
                 case "flag":
                     if(args.length == 2){
-                        return getFlagValue(args[1]);
+                        return getFlagValue(args[1], "[undefined]");
+                    }
+                    if(args.length == 3){
+                        return  getFlagValue(args[1], args[2]);
                     }
                     return "[Invalid placeholder syntax]";
+                case "width":
+                    return getWidth();
+                case "length":
+                    return getLength();
+                case "height":
+                    return getHeight();
+                case "bottom":
+                    if(args.length == 2){
+                        try{
+                            return getBottom(Integer.parseInt(args[1]));
+                        }catch (NumberFormatException e){
+                            return "[Invalid placeholder syntax]";
+                        }
+                    }
+                    return getBottom(0);
+                case "top":
+                    if(args.length == 2){
+                        try{
+                            return getTop(Integer.parseInt(args[1]));
+                        }catch (NumberFormatException e){
+                            return "[Invalid placeholder syntax]";
+                        }
+                    }
+                    return getTop(0);
             }
         }
         return "[Unknown placeholder]";
+    }
+
+    private String getWidth(){
+        return "" + (region.getMaximumPoint().getX() + 1 - region.getMinimumPoint().getX());
+    }
+
+    private  String getLength(){
+        return "" + (region.getMaximumPoint().getZ() + 1 - region.getMinimumPoint().getZ());
+    }
+
+    private String getHeight(){
+        return "" + (region.getMaximumPoint().getY() + 1 - region.getMinimumPoint().getY());
+    }
+
+    private String getBottom(int offset){
+        return "" + (region.getMinimumPoint().getY() - offset);
+    }
+
+    private String getTop(int offset){
+        return "" + (region.getMaximumPoint().getY() + 1 - offset);
     }
 
     private String getName(boolean forceId){
@@ -133,16 +181,16 @@ public class RegionStringLookup extends StringLookupWrapper {
         return area2D.toString();
     }
 
-    private String getFlagValue(String flagname){
+    private String getFlagValue(String flagname, String fallback){
         if(flagReg == null)
             flagReg = WorldGuard.getInstance().getFlagRegistry();
         Flag<?> flag = flagReg.get(flagname);
         if(flag == null)
-            return "[flag not registered]";
+            return fallback;
         Object flagValue = region.getFlag(flag);
         if(flagValue == null){
             if(flag.getDefault() == null){
-                return "[undefined]";
+                return fallback;
             }else{
                 return BlueBridgeUtils.escapeHtml(flag.getDefault().toString());
             }

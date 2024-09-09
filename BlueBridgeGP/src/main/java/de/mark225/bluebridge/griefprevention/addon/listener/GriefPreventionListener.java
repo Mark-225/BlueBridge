17fd1779c029cd6f943258a43a7c3de0bc6d48bf
Mark_225 <mark@mark225.de>
2024-09-09 21:38:46 +0200

    Replace deprecated event

--- BlueBridgeGP/src/main/java/de/mark225/bluebridge/griefprevention/addon/listener/GriefPreventionListener.java
+++ BlueBridgeGP/src/main/java/de/mark225/bluebridge/griefprevention/addon/listener/GriefPreventionListener.java
@@ -3,10 +3,7 @@ package de.mark225.bluebridge.griefprevention.addon.listener;
 import de.mark225.bluebridge.core.config.BlueBridgeConfig;
 import de.mark225.bluebridge.griefprevention.BlueBridgeGP;
 import me.ryanhamshire.GriefPrevention.Claim;
-import me.ryanhamshire.GriefPrevention.events.ClaimCreatedEvent;
-import me.ryanhamshire.GriefPrevention.events.ClaimDeletedEvent;
-import me.ryanhamshire.GriefPrevention.events.ClaimExtendEvent;
-import me.ryanhamshire.GriefPrevention.events.ClaimModifiedEvent;
+import me.ryanhamshire.GriefPrevention.events.*;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -25,9 +22,9 @@ public class GriefPreventionListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onClaimModified(ClaimModifiedEvent e) {
+    public void onClaimModified(ClaimResizeEvent e) {
         if (BlueBridgeConfig.debug())
-            BlueBridgeGP.getInstance().getLogger().log(Level.INFO, "Claim modified " + e.getFrom().getID());
+            BlueBridgeGP.getInstance().getLogger().log(Level.INFO, "Claim resized " + e.getFrom().getID());
         if (e.isCancelled()) return;
         scheduleUpdate(e.getFrom());
     }
